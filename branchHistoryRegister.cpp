352eb324f841e58822d5912cf1c4316aa08bd08f
Sean Blucker <smblucker@outlook.com>
2017-04-03 11:39:48 -0400

    Defining branchHistoryRegister class

--- branchHistoryRegister.cpp
+++ branchHistoryRegister.cpp
@@ -6,5 +6,61 @@ using namespace std;
 
 namespace csci5814
 {
-    
+    branchHistoryRegister::branchHistoryRegister()
+    {
+        value = 0;
+        size = 3;
+    }
+
+    int branchHistoryRegister::getValue()
+    {
+        return value;
+    }
+
+    int branchHistoryRegister::getSize()
+    {
+        return size;
+    }
+
+    void branchHistoryRegister::setValue(int setValue)
+    {
+        int mask = (1 << getSize()) - 1;
+        value = setValue & mask;
+    }
+
+    void branchHistoryRegister::incrementValue()
+    {
+        value += 1;
+    }
+
+    void branchHistoryRegister::decrementValue()
+    {
+        value -= 1;
+    }
+
+    void branchHistoryRegister::shiftValueLeft()
+    {
+        value <<= 1;
+    }
+
+    void branchHistoryRegister::shiftValueRight()
+    {
+        value >>= 1;
+    }
+
+    void branchHistoryRegister::updateTaken()
+    {
+        shiftValueLeft();
+        incrementValue();
+        int mask = (1 << getSize()) - 1;
+        value = value & mask;
+    }
+
+    void branchHistoryRegister::updateNotTaken()
+    {
+        shiftValueRight();
+        decrementValue();
+        int mask = (1 << getSize()) - 1;
+        value = value & mask;
+    }
 }
--- branchHistoryRegister.h
+++ branchHistoryRegister.h
@@ -9,7 +9,18 @@ namespace csci5814
     class branchHistoryRegister
     {
     private:
+        int value;
+        int size;
     public:
+        int getValue();
+        int getSize();
+        void setValue(int setValue);
+        void incrementValue();
+        void decrementValue();
+        void shiftValueLeft();
+        void shiftValueRight();
+        void updateTaken();
+        void updateNotTaken();
     };
 }
 
